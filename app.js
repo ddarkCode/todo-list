19ad6952ba393bc35b12e702a1c6ac97fd06238c
Nnamani Ugochukwu Frederick <royal.ugoh@gmail.com>
2022-06-25 18:53:06 +0100

    Add Connect to mongoDB database, Create gitignore and Styles

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const _ = require('lodash');
 const { connect, model, Schema } = require('mongoose');
 
 const day = require('./date')();
@@ -21,6 +22,16 @@ connect(
   }
 );
 
+const daysOfWeek = [
+  'Sunday,',
+  'Monday,',
+  'Tuesday,',
+  'Wednesday,',
+  'Thursday,',
+  'Friday,',
+  'Saturday,',
+];
+
 const todolistSchema = new Schema({
   name: String,
 });
@@ -38,6 +49,13 @@ const item3 = new Todo({
 });
 
 const todos = [item1, item2, item3];
+
+const listSchema = new Schema({
+  name: String,
+  lists: [todolistSchema],
+});
+
+const List = model('List', listSchema);
 
 app
   .route('/')
@@ -63,18 +81,95 @@ app
   })
   .post((req, res) => {
     console.log(req.body);
-    const { todo } = req.body;
-    const newTodo = new Todo({
-      name: todo,
-    });
-    newTodo.save((err) => {
+    const { todo, listTitle } = req.body;
+
+    if (daysOfWeek.includes(listTitle)) {
+      const newTodo = new Todo({
+        name: todo,
+      });
+      newTodo.save((err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.redirect('/');
+        }
+      });
+    } else {
+      List.findOne({ name: listTitle }, (err, foundList) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const newCustomItem = new List({
+            name: todo,
+          });
+          foundList.lists.push(newCustomItem);
+          foundList.save((err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.redirect('/' + listTitle);
+            }
+          });
+        }
+      });
+    }
+  });
+
+app.get('/:customList', (req, res) => {
+  let { customList } = req.params;
+  customList = _.capitalize(customList);
+  List.findOne({ name: customList }, (err, foundList) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (!foundList) {
+        const newCustomList = new List({
+          name: customList,
+          lists: todos,
+        });
+        newCustomList.save((err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.redirect('/' + customList);
+          }
+        });
+      } else {
+        res.render('list', {
+          listTitle: foundList.name,
+          items: foundList.lists,
+        });
+      }
+    }
+  });
+});
+
+app.post('/delete', (req, res) => {
+  let { todoId, listTitle } = req.body;
+  listTitle = listTitle.split(' ')[0];
+
+  if (daysOfWeek.includes(listTitle)) {
+    Todo.findByIdAndDelete(todoId, (err, deletedTodo) => {
       if (err) {
         console.log(err);
       } else {
         res.redirect('/');
       }
     });
-  });
+  } else {
+    List.findOneAndUpdate(
+      { name: listTitle },
+      { $pull: { lists: { _id: todoId } } },
+      (err, updatedList) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.redirect('/' + listTitle);
+        }
+      }
+    );
+  }
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
